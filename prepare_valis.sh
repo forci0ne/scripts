580b39ea6f1b383a874316078b6aa25f6d5cc006
forci0ne <peter@deboer.vc>
2025-05-05 09:45:14 +0200

    updated links

--- prepare_valis.sh
+++ prepare_valis.sh
@@ -67,7 +67,7 @@ echo "Downloading CLI file..."
 if [ -f "vcli" ]; then
   rm -f vcli
 fi
-wget -q https://github.com/valis-team/tockchain/raw/refs/heads/main/CoreTestnet/CLI/tockchain-vusd-cli-v2.0.1
+wget -q https://github.com/valis-team/tockchain/raw/refs/heads/main/CoreTestnet/CLI/tockchain-vusd-core-cli-v2.1.0
 mv tockchain-vusd-cli-v2.0.1 vcli
 chmod +x vcli
 
